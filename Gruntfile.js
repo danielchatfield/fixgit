2933cd39861e3dc7b029496bf189c831d0209adc
Daniel Chatfield <chatfielddaniel@gmail.com>
2013-08-11 16:16:02 +0100

    Working fixgit

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      bin: {
+        src: ['bin/gitfix']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      bin: {
+        files: '<%= jshint.bin.src %>',
+        tasks: ['jshint:lib']
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
